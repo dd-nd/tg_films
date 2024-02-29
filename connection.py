dfc4e6fe94b52733ad4e1bacde461d58e0ef6f04
dd-nd <lya.yop@yandex.ru>
2024-02-29 17:44:01 +0300

    drop за ненужностью

--- connection.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect(dbname='films_py', user='postgres', password='123', host='localhost')
-except:
-    print('Не удается подключиться к базе данных')
